fe35659980272a3b7802ca2c6f0a26ba2617835e
mtmrstr <106470191+mtmrstr@users.noreply.github.com>
2022-07-24 04:40:32 +0000

    MC

--- app/controllers/master/products_controller.rb
+++ app/controllers/master/products_controller.rb
@@ -4,28 +4,28 @@ class Master::ProductsController < ApplicationController
     @product = Product.new
     @genres = Genre.all
   end
-  
+
   def create
     @product = Product.new(product_params)
     @product.save
     redirect_to master_products_path
   end
-  
+
   def index
     @products = Product.all.page(params[:page]).per(10)
   end
-  
+
   def show
     @product = Product.find_by(id: params[:id])
   end
-  
+
   private
   def product_params
     params.require(:product).permit(:image, :name, :description, :genre_id, :price, :sale_status)
   end
-  
+
   def set_genres
     @genres = Genre.all
   end
-    
+
 end
