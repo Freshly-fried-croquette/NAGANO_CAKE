2c19544fcd2148871216265ac9dd9f2ab2018948
mtmrstr <106470191+mtmrstr@users.noreply.github.com>
2022-07-26 09:40:40 +0000

    ステータス修正

--- app/controllers/master/orders_controller.rb
+++ app/controllers/master/orders_controller.rb
@@ -3,9 +3,14 @@ class Master::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
+
+ def update
+      @order = Order.find(params[:id])
+      @order.update(order_params)
+ end
+
   private
    def order_params
-     params.require(:order).permit(:quantity, :price)
+     params.require(:order).permit(:quantity, :price ,:order_status)
    end
-
 end
