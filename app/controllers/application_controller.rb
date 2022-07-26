562998512f0412c6b015ddc9dd9cac83dbeaadbd
kawatake0329 <104962638+kawatake0329@users.noreply.github.com>
2022-07-26 11:29:17 +0000

    [add]master:ログアウト先追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     private
-    def configure_permitted_parameters 
-        devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number]) 
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number])
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        if resource_or_scope == :admin_admin_user
+          new_admin_admin_user_session_path
+        else
+          new_master_session_path
+        end
     end
 end
